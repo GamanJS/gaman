a9275810d97dd81dc4bf0369c0758d9a9d14786d
Angga7Togk <kiplihode123321@gmail.com>
2025-11-11 13:14:17 +0700

    [feat] premerger

--- test/src/index.ts
+++ test/src/index.ts
@@ -3,64 +3,18 @@ import { defineBootstrap } from '@gaman/core';
 import AppRoutes from './routes/AppRoutes';
 import { staticServe } from '@gaman/static';
 import AppMiddleware from './middlewares/AppMiddleware';
-import { cors } from '@gaman/cors';
 import { WebsocketGateway } from '@gaman/websocket';
-import { session } from '@gaman/session';
-import { edge } from '@gaman/edge';
-import EdgeHandler from './EdgeHandler';
-import { jwt } from '@gaman/jwt';
 import JwtRoutes from './routes/JwtRoutes';
 import { jwtAuthMiddleware } from './middlewares/JwtAuthMiddleware';
 
 defineBootstrap(async (app) => {
 	app.mount(
 		AppRoutes,
-		edge({
-			handler: EdgeHandler,
-		}),
-		cors({
-			origin: ['http://127.0.0.1:5500'],
-			credentials: true,
-		}),
 		staticServe(),
 		AppMiddleware(),
 		jwtAuthMiddleware(),
 	);
-	
 
-	const sessionData: Record<string, any> = {};
-	app.mount(
-		session({
-			crossSite: true,
-			store: {
-				async delete(data) {
-					delete sessionData[data.sid];
-				},
-				async get(data) {
-					return sessionData[data.sid];
-				},
-				async set(data) {
-					sessionData[data.sid] = data.payload;
-				},
-			},
-		}),
-	);
-
-	app.mount(
-		jwt({
-			secret: 'secret',
-			header: 'Authorization',
-			required: false,
-			includes: ['/jwt/unprotected', '/jwt/token'],
-		}),
-		jwt({
-			secret: 'secret',
-			header: 'Authorization',
-			required: true,
-			includes: ['/jwt/protected'],
-			excludes: ['/jwt/unprotected', '/jwt/token'],
-		}),
-	);
 
 	app.mount(JwtRoutes);
 
