7db3ac4f6909b21a0eb244abe2a9639fcb787193
Angga7Togk <kiplihode123321@gmail.com>
2026-05-18 19:13:39 +0700

    fix(db): refactor composeMigration

--- packages/db/src/compose/migration.ts
+++ packages/db/src/compose/migration.ts
@@ -1,8 +1,48 @@
-import type { MigrationBuilder } from '../migration';
+import { MigrationModel } from '../database/MigrationModel';
+import { MigrationBuilder } from '../migration';
+import { Logger } from 'gaman/utils';
 
-export function composeMigration(handlers: {
+export async function composeMigration(handlers: {
 	up: (m: MigrationBuilder) => Promise<void> | void;
 	down: (m: MigrationBuilder) => Promise<void> | void;
 }) {
-	return handlers;
+	await MigrationModel.sync();
+
+	return {
+		async runUp(filename: string) {
+			const found = await MigrationModel.findOne('name', filename);
+
+			if (found) {
+				Logger.error(
+					`Migration [${filename}] already executed. Use '--fresh' to drop and up again.`,
+				);
+				return;
+			}
+
+			Logger.info(`Migrating: ${filename}`);
+			await handlers.up(new MigrationBuilder());
+
+			if (!found) {
+				await MigrationModel.create({ name: filename });
+			}
+			Logger.info(`Migrated: ${filename}`);
+		},
+
+		async runDown(filename: string, force: boolean = false) {
+			const found = await MigrationModel.findOne('name', filename);
+
+			if (!found && !force) {
+				Logger.warn(
+					`Skip Rollback: [${filename}] has not been executed. Use '--force' to re-run.`,
+				);
+				return;
+			}
+
+			Logger.info(`Rolling back: ${filename}`);
+			await handlers.down(new MigrationBuilder());
+
+			if(found) await MigrationModel.delete(found.id);
+			Logger.info(`Rolled back: ${filename}`);
+		},
+	};
 }
